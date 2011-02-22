8195f0d5708bbf039ac84e06b0c438c0c94aad4b
rehan <rehan@ip-10-162-234-17.us-west-1.compute.internal>
2011-02-22 10:20:49 +0000

    adding pagination code

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -4,7 +4,16 @@ class EmailsController < ApplicationController
   def inbox
     #@email = Email.find(params[:username])
    # @emails = Email.find_by_username(params[:username])
-     @emails = Email.where("username = ?", params[:username])
+     #@emails = Email.where("username = ?", params[:username])
+    # @user = Email.where("username = ?", params[:username])
+     #@emails = Email.paginate_by_user_id("#{@user.id}", :page => params[:page], :order => 'updated_at DESC', :per_page => 20)
+    #@user_emails = Email.where("username = ?", params[:username])
+    #@emails = Email.paginate_by_username("#{params[:username]}", :page => params[:page], :order => 'updated_at DESC', :per_page => 20)
+
+
+    @user_emails = Email.where("username = ?", params[:username])
+    @emails = @user_emails.paginate :page => params[:page], :order => 'created_at DESC', :per_page => 10
+
     respond_to do |format|
       format.html # inbox.html.erb
       format.xml  { render :xml => @email }
@@ -18,9 +27,11 @@ class EmailsController < ApplicationController
 
 
         if (@message.multipart?)
-          @body = @message.text_part.decoded
+#          @body = @message.text_part.decoded
+           @body = @message.html_part
         else
           @body = @message.decoded
+          @body = @body.to_s
         end
 
         respond_to do |format|
