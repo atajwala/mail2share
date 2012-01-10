f97efb1d810fa80d0025a66db39c7cfc65856366
Aslam Tajwala <atajwala@gmail.com>
2012-01-09 18:11:25 -0800

    added some indentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,44 +1,44 @@
 class User < ActiveRecord::Base
-	has_many :emails
-	
-	acts_as_authentic do |c|
-	  c.require_password_confirmation = false
-		#c.validate_email_field = false
-		#c.validate_login_field = false
-		#c.validate_password_field = false
-		c.email_field = 'user_email'
-		#c.password_field = 'password'
-		#c.login_field = 'username'
-	end
-	
-	def activate!
-	    self.active = true
-	    save
-	end
-	
-	def deactivate!
-		  self.active = false
-		  save
-	end
-	
-	def deliver_activation_instructions!
-	    reset_perishable_token!
-			UserMailer.activation_email(self).deliver
-	end
+  has_many :emails
+  
+  acts_as_authentic do |c|
+    c.require_password_confirmation = false
+    #c.validate_email_field = false
+    #c.validate_login_field = false
+    #c.validate_password_field = false
+    c.email_field = 'user_email'
+    #c.password_field = 'password'
+    #c.login_field = 'username'
+  end
+  
+  def activate!
+      self.active = true
+      save
+  end
+  
+  def deactivate!
+      self.active = false
+      save
+  end
+  
+  def deliver_activation_instructions!
+      reset_perishable_token!
+      UserMailer.activation_email(self).deliver
+  end
 
-	def deliver_welcome!
-	  reset_perishable_token!
-		UserMailer.welcome_email(self).deliver
-	end
-	
-	def deliver_password_reset_instructions!
-		reset_perishable_token!  
-		UserMailer.password_reset_email(self).deliver
-	end
-	
-	define_index do
+  def deliver_welcome!
+    reset_perishable_token!
+    UserMailer.welcome_email(self).deliver
+  end
+  
+  def deliver_password_reset_instructions!
+    reset_perishable_token!  
+    UserMailer.password_reset_email(self).deliver
+  end
+  
+  define_index do
     indexes [fullname, username], :as => :name
     indexes location
   end
-	
+  
 end
